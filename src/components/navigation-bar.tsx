f8a001d88da2cc9c4cd40ffd58c9f7fa95a30a51
Dennise Catolos <dentolos19@gmail.com>
2025-03-13 13:51:18 +0800

    Improved responsiveness

--- src/components/navigation-bar.tsx
+++ src/components/navigation-bar.tsx
@@ -1,27 +1,54 @@
 import clsx from "clsx";
+import { MenuIcon } from "lucide-react";
 import Link from "next/link";
+
+const links = [
+  {
+    name: "Featured",
+    url: "/posts/featured",
+  },
+  {
+    name: "Clubs",
+    url: "/posts/clubs",
+  },
+  {
+    name: "Tech",
+    url: "/posts/tech",
+  },
+  {
+    name: "Guides",
+    url: "/posts/guides",
+  },
+];
 
 export default function NavigationBar(props: { className?: string }) {
   return (
     <nav className={clsx("navbar bg-base-300 rounded-box", props.className)}>
       <div className={"navbar-start"}>
+        <div className={"lg:hidden dropdown"}>
+          <div tabIndex={0} className={"btn btn-square btn-ghost"} role={"button"}>
+            <MenuIcon />
+          </div>
+          <ul tabIndex={0} className={"dropdown-content menu bg-base-300 rounded-box w-48 p-2 shadow-lg"}>
+            {links.map((link) => (
+              <li key={link.url}>
+                <Link href={link.url}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
         <Link className={"btn btn-ghost"} href={"/"}>
           <span className={"font-bold text-lg"}>SIT Chapters</span>
         </Link>
       </div>
       <div className={"navbar-center"}>
-        <Link className={"btn btn-ghost"} href={"#"}>
-          Featured
-        </Link>
-        <Link className={"btn btn-ghost"} href={"#"}>
-          Clubs
-        </Link>
-        <Link className={"btn btn-ghost"} href={"#"}>
-          Tech
-        </Link>
-        <Link className={"btn btn-ghost"} href={"#"}>
-          Guides
-        </Link>
+        <ul className={"max-lg:hidden menu menu-horizontal"}>
+          {links.map((link) => (
+            <li key={link.url}>
+              <Link href={link.url}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
       <div className={"navbar-end"}>
         <Link className={"btn btn-ghost"} href={"/about"}>
