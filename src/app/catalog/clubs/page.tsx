0cbcbebc80a58ed89382636075b0d335d2691dd2
Dennise Catolos <dentolos19@gmail.com>
2025-03-31 18:31:46 +0800

    Added a club into catalog

--- src/app/catalog/clubs/page.tsx
+++ src/app/catalog/clubs/page.tsx
@@ -2,42 +2,42 @@ import Link from "next/link";
 
 const clubs = [
   {
-    name: "Technopreneurship Club",
+    name: "NYP Technopreneurs",
     description: "Learn to be a technopreneur!",
     logo: "/clubs/nyptech.png",
-    url: "https://nyptech.club",
-  },
-  {
-    name: "Google Developer Student Club",
-    description: "Be part of the innovation!",
-    logo: "/placeholder.webp",
     url: "https://nyptech.club",
   },
   {
     name: "NYP Cloud",
     description: "Learning to power the world!",
     logo: "/placeholder.webp",
-    url: "https://nyptech.club",
+    url: "#",
   },
   {
     name: "NYP Blockchain",
     description: "Join the us within the new web!",
     logo: "/placeholder.webp",
-    url: "https://nyp-lit.github.io",
+    url: "#",
   },
   {
     name: "NYP LIT",
-    description: "Join the ladies in tech!",
-    logo: "/placeholder.webp",
+    description: "Empowering women of our generation!",
+    logo: "/clubs/nyplit.jpg",
     url: "https://nyp-lit.github.io",
+  },
+  {
+    name: "Google Developer Student Club",
+    description: "Be part of the innovation!",
+    logo: "/placeholder.webp",
+    url: "#",
   },
 ];
 
 export default function Page() {
   return (
-    <main className={"p-4 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"}>
+    <main className={"grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"}>
       {clubs.map((club, index) => (
-        <Link key={index} className={"card bg-base-300 hover:bg-base-200 transition"} href={"#"}>
+        <Link key={index} className={"card bg-base-300 hover:bg-base-200 transition"} target={"_blank"} href={club.url}>
           <figure>
             <img className={"aspect-square object-cover"} src={club.logo} />
           </figure>
