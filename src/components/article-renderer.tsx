6f0bfff846d1bd7039b567ee7a4c6bc62150e90b
Dennise Catolos <dentolos19@gmail.com>
2025-04-11 21:04:14 +0800

    Improved article renderer

--- src/components/article-renderer.tsx
+++ src/components/article-renderer.tsx
@@ -3,8 +3,12 @@ import dynamic from "next/dynamic";
 import Link from "next/link";
 import { ExtendedRecordMap } from "notion-types";
 import { NotionRenderer } from "react-notion-x";
-import "react-notion-x/src/styles.css";
 import styles from "./article-renderer.module.css";
+
+// Notion Renderer Styles
+import "katex/dist/katex.min.css";
+import "prismjs/themes/prism-tomorrow.css";
+import "react-notion-x/src/styles.css";
 
 const Code = dynamic(() => import("react-notion-x/build/third-party/code").then((m) => m.Code));
 const Collection = dynamic(() => import("react-notion-x/build/third-party/collection").then((m) => m.Collection));
